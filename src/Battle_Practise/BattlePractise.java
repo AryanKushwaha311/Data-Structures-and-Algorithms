11a4efb915753580b53d21e1b748c98bb9c0e310
Charliee311 <codewithkushwaha@gmail.com>
2026-03-28 13:14:20 +0530

    new Files added

--- src/Battle_Practise/BattlePractise.java
+++ src/Battle_Practise/BattlePractise.java
@@ -2,25 +2,44 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class BattlePractise{
-    
+    // Question - 1
+    public static String lowerToUpperCase(String str){
+        str=str.toLowerCase();
+        String newStr = "";
 
-    // Question - 12
-    public static int[] zeroAtEnd(int[] arr){
-        int j = 0;
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i] == 0){
-                int temp = arr[i];
-                arr[i] = arr[arr.length-1-j];
-                arr[arr.length - 1] = temp;
-                j++;
+        for (int i = 0;  i< str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 97 || ch <= 122){
+                ch = (char)(ch - 32);
+                newStr = newStr + ch;
             }
         }
-        return arr;
+        return newStr;
     }
 
+    //Question-2
+    public static void countFreq(String str){
+        int max = Integer.MIN_VALUE;
+        char freq = ' ';
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)){
+                    count++;
+                }
+            }
+            if(count > max){
+                max = count;
+                freq =str.charAt(i);
+            }
+        }
+        System.out.println("Alphabet that appears most: "+freq);
+        System.out.println("Number of time it appears: "+max);
+    }
     public static void main(String[] args) {
-        int[] arr = {11,0,22,0,33,0,0,44};
-        int[] result = zeroAtEnd(arr);
-        System.out.println(Arrays.toString(result));
+
+        countFreq("aryananynannn");
+
     }
 }
